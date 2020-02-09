3540a3e5906f175e77b1b19d4a780edc8e17ca56
jaspreet <jaspreet.singh@allocatesoftware.com>
2020-02-09 19:14:12 +0530

    Updates

---
+++ precache-manifest.2be15eb5abb17bb2074cf9bcca3b8f77.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f40c0ea4bc097fd398fbcdaa816905fc",
+    "url": "/e-kart/index.html"
+  },
+  {
+    "revision": "6afa4f53ba2c435e433e",
+    "url": "/e-kart/static/css/2.6d7d54d2.chunk.css"
+  },
+  {
+    "revision": "6407a1134cb495e852de",
+    "url": "/e-kart/static/css/main.8055daf0.chunk.css"
+  },
+  {
+    "revision": "6afa4f53ba2c435e433e",
+    "url": "/e-kart/static/js/2.df3b34a6.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/e-kart/static/js/2.df3b34a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6407a1134cb495e852de",
+    "url": "/e-kart/static/js/main.35dc9c0a.chunk.js"
+  },
+  {
+    "revision": "4b6c4d88230d9dedcadb",
+    "url": "/e-kart/static/js/runtime-main.43944ab7.js"
+  },
+  {
+    "revision": "72cf2e802eb32ca14d9868fb2005909c",
+    "url": "/e-kart/static/media/Verdana.72cf2e80.ttf"
+  },
+  {
+    "revision": "9dfc9092b8551f78b07a2ca36322c71a",
+    "url": "/e-kart/static/media/boss.9dfc9092.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/e-kart/static/media/logo.5d5d9eef.svg"
+  }
+]);
